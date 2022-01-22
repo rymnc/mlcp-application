45e33113c953b46188382270a38400ed30942319
rymnc <aaryamannchallani7@gmail.com>
2022-01-22 20:47:14 +0530

    feat: initialize

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+
+import type {NextPage} from 'next'
+import Head from 'next/head';
+import Hero from '../components/Hero';
+
+const Home: NextPage = () => {
+  return (
+    <Head>
+      <Hero/>
+    </Head>
+  )
+}
+
+export default Home;
