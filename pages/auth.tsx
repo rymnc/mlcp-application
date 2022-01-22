95670e4134c7c78fd2ba7d44bdd6816d1305d959
rymnc <aaryamannchallani7@gmail.com>
2022-01-23 02:05:23 +0530

    fix: type of component

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,7 +1,7 @@
 import AuthProfile from "../components/Auth";
-import type { NextPage } from "next";
+import type { NextComponentType } from "next";
 
-const Auth: NextPage = () => {
+const Auth: NextComponentType = () => {
   return (
     <div className="w-full items-center justify-center flex">
       <div className="w-full sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/3 2xl:w-1/3 mx-4">
